0350b955cc374f5e06a568eb658266daa6d1b9a9
Jean <acalto2@gmail.com>
2023-12-12 21:02:33 +0100

    9. Pointers

--- main.go
+++ main.go
@@ -1,23 +1,20 @@
 package main
 
-import "fmt"
+import "log"
 
 func main() {
-	fmt.Println("Hello, world")
+	var myString string
+	myString = "Green"
 
-	var whatToSay string
-	var i int
+	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("aftec call myString is set to", myString)
 
-	whatToSay = "Goodbay, cruel world"
-	fmt.Println(whatToSay)
-
-	i = 7
-	fmt.Println("i is seto to", i)
-	whatWasSaid, theOtherThing := saySomething()
-
-	fmt.Println("The function returned", whatWasSaid, theOtherThing)
 }
 
-func saySomething() (string, string) {
-	return "something", "else"
+func changeUsingPointer(s *string) {
+	log.Println("s is set to", s)
+
+	newValue := "red"
+	*s = newValue
 }
