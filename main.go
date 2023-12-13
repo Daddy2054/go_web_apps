d801ef5c9904ccc11126d62f269555d5f6764f6f
Jean <acalto2@gmail.com>
2023-12-13 19:25:32 +0100

    12. Other data structures: Maps and Slices

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
-import (
-	"log"
-)
-
-type myStruct struct {
-	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
+import "log"
 
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	names := []string{"one", "fish", "cat"}
+	log.Println(names)
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
-	}
-	log.Println("myVar is set to: ", myVar.printFirstName())
-	log.Println("myVar2 is set to: ", myVar2.printFirstName())
+	// var mySlice []string
+	// mySlice = append(mySlice, "James")
+	// mySlice = append(mySlice, "Bond")
+	// mySlice = append(mySlice, "Penny")
+
+	// log.Println(mySlice)
+
+	// numbers := []int{1, 2, 3, 4, 5}
+
+	// log.Println(numbers)
+	// log.Println(numbers[0:2])
+	// myMap := make(map[string]string)
+
+	// myMap["dog"]="Simpson"
+	// myMap["other-dog"]="Cassie"
+
+	// log.Println(myMap["dog"])
+	// log.Println(myMap["other-dog"])
+	// var myString string
+	// var myInt int
+
+	// myString = "Hi"
+	// myInt = 11
+
+	// mySecondString := "another string"
+
+	// log.Println(myString, mySecondString, myInt)
 }
